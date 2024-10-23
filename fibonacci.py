4049d11cf4aa95fb616f0bf5f34059f58f05daca
vanshrane27 <157192546+vanshrane27@users.noreply.github.com>
2024-10-23 16:26:25 +0530

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -7,9 +7,6 @@ def fibonacci(n):
         a, b = b, a + b
 
 if __name__ == "__main__":
-    if sys.version_info[0] < 3: #Check Python version on system
-        n = int(raw_input("Enter the max number for the Fibonacci Series: ")) #Python2
-    else:
-        n = int(input("Enter the max number for the Fibonacci Series: ")) #Python3
+    n = int(sys.stdin.read().strip())
 
-    print("Fibonacci Series: " + " ".join(str(num) for num in fibonacci(n)))
+    print(" ".join(str(num) for num in fibonacci(n)))
--- reverse.py
+++ reverse.py
@@ -4,9 +4,6 @@ def reverse_number(n):
     return int(str(n)[::-1])
 
 if __name__ == "__main__":
-    if sys.version_info[0] < 3: #Check Python version on the system
-        n = int(raw_input("Enter a number: ")) #Python2
-    else:
-        n = int(input("Enter a number: ")) #Python3
+    n = int(sys.stdin.read().strip())
 
-    print("Number in Reverse Order:", reverse_number(n))
+    print(reverse_number(n))
